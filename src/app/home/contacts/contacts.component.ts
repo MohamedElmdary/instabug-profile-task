9032b9d37de5586abc5bf95d5699d7f0507800f5
MohamedElmdary <engm5081@gmail.com>
2020-05-13 18:01:47 +0200

    finished main features

--- src/app/home/contacts/contacts.component.ts
+++ src/app/home/contacts/contacts.component.ts
@@ -12,6 +12,8 @@ import { FormBuilder, Validators, FormControl } from '@angular/forms';
 export class ContactsComponent {
   isSubmited = false;
 
+  attachements: File[] = [];
+
   messageForm = this.fb.group({
     email: ['', [Validators.email, Validators.required]],
     subject: ['', [Validators.required, Validators.minLength(5)]],
@@ -36,5 +38,18 @@ export class ContactsComponent {
     // some logic to handle submition
     this.isSubmited = true;
     console.log(this.messageForm.value);
+    console.log(this.attachements);
+  }
+
+  removeAttachement(idx: number) {
+    this.attachements.splice(idx, 1);
+  }
+
+  onAddFiles(fl: FileList) {
+    for (let i = 0; i < fl.length; i++) {
+      if (!this.attachements.some(atch => atch.name === fl.item(i).name)) {
+        this.attachements.push(fl.item(i));
+      }
+    }
   }
 }
